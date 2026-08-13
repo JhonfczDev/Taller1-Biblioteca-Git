a54f516317bf089e633a470a9ce0655cd7307597
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-08-13 12:28:12 -0500

    feat: implement Client READ method

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -10,29 +10,47 @@ public class Main {
     static void createClient(){
         Client newClient = new Client();
         
-        System.out.println("__________AGREGAR CLIENTE__________");
-        System.out.println("-A continuacion ingrese los datos del cliente a agregar:");
+        System.out.println("          __________AGREGAR CLIENTE__________");
+        System.out.println("-A continuacion ingrese los datos del cliente a agregar:\n");
         
-        System.out.println("-Ingrese la identificacion:");
-        System.out.print("*"); newClient.setId(sc.nextLine());
+        System.out.print("-Ingrese la identificacion: "); newClient.setId(sc.nextLine());
         
-        System.out.println("-Ingrese el nombre:");
-        System.out.print("*"); newClient.setName(sc.nextLine());
+        System.out.print("-Ingrese el nombre: "); newClient.setName(sc.nextLine());
         
-        System.out.println("-Ingrese el numero de telefono:");
-        System.out.print("*"); newClient.setPhoneNumber(sc.nextLine());
+        System.out.print("-Ingrese el numero de telefono: "); newClient.setPhoneNumber(sc.nextLine());
         
-        System.out.println("-Ingrese el email:");
-        System.out.print("*"); newClient.setPhoneNumber(sc.nextLine());
+        System.out.print("-Ingrese el email: "); newClient.setEmail(sc.nextLine());
         
         clientes.add(newClient);
-        
-        System.out.println("\n-Cliente agregado exitosamente.");
+        System.out.println("\n-Cliente agregado exitosamente.\n");
         
         return;
     }
+    
+    static void readClient(){
+        
+        String resultado=" ";
+        int ind=1;
+        
+        for (Client client : clientes) {
+            resultado += "CLIENTE "+ind+".\n" 
+                    +"\n-Identificacion: "+client.getId()
+                    +"\n-Nombre: "+client.getName()
+                    +"\n-Telefono: "+client.getPhoneNumber()
+                    +"\n-Email: "+client.getEmail()
+                    +"\n";
+                    
+                    ind+=1;
+        }
+        
+        System.out.println("          __________CONSULTAR CLIENTES__________");
+        System.out.println("-Listado de clientes registrados:\n");
+        System.out.println(resultado);
+        
+    };
 
     public static void main(String[] args) {
         createClient();
+        readClient();
     }
 }
