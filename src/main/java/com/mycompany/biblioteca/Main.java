a9bb38e61f14068f8442e036fd673b9496ccbec6
Jhon Fredy Chinchilla <jhonfchinchilla@unicesar.edu.co>
2026-08-13 12:05:14 -0500

    feat: implement Client CREATE method

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -1,16 +1,38 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.biblioteca;
 
-/**
- *
- * @author ESTUDIANTES
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static ArrayList<Client> clientes = new ArrayList<>(); 
+    static Scanner sc = new Scanner(System.in);
+    
+    static void createClient(){
+        Client newClient = new Client();
+        
+        System.out.println("__________AGREGAR CLIENTE__________");
+        System.out.println("-A continuacion ingrese los datos del cliente a agregar:");
+        
+        System.out.println("-Ingrese la identificacion:");
+        System.out.print("*"); newClient.setId(sc.nextLine());
+        
+        System.out.println("-Ingrese el nombre:");
+        System.out.print("*"); newClient.setName(sc.nextLine());
+        
+        System.out.println("-Ingrese el numero de telefono:");
+        System.out.print("*"); newClient.setPhoneNumber(sc.nextLine());
+        
+        System.out.println("-Ingrese el email:");
+        System.out.print("*"); newClient.setPhoneNumber(sc.nextLine());
+        
+        clientes.add(newClient);
+        
+        System.out.println("\n-Cliente agregado exitosamente.");
+        
+        return;
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        createClient();
     }
 }
--- src/main/java/com/mycompany/biblioteca/Person.java
+++ src/main/java/com/mycompany/biblioteca/Person.java
@@ -1,8 +1,46 @@
 package com.mycompany.biblioteca;
 
 public class Person {
-    String id;
-    String name;
-    String phoneNumber;
+    
+    private String id;
+    private String name;
+    private String phoneNumber;
+
+    public Person() {
+    }
+
+    public Person(String id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+    
+    
     
 }
